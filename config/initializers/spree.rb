f034c041781535db4aa1a1dbd311c48ec414ef91
Alexander Wenzowski <alexander@wenzowski.com>
2013-03-22 23:29:29 -0400

    Stripe does not currently support separate auth and capture

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -9,6 +9,7 @@ Spree.config do |config|
   # Example:
   # Uncomment to override the default site name.
   # config.site_name = "Spree Demo Site"
+  config.auto_capture = true
 end
 
 Spree.user_class = "Spree::User"
